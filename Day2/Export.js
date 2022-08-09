4bdf06e45696ca8cd2fdab566e1c5ab9734ee665
Niraj Singh <102036558+Starniraj@users.noreply.github.com>
2022-08-09 19:24:34 +0530

    Add files via upload

---
+++ Day2/Export.js
@@ -0,0 +1,9 @@
+function sum (req,res){
+    res.send("Sum")
+}
+// function name(req,res) {
+//     res.send('Hi I am Niraj ')
+// }
+
+module.exports = sum;//default export
+// module.exports = {name};//named export
